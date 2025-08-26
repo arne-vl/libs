d02935643faa6caa6f0779319e930a1f7ae56b29
Arne Van Looveren <arne-vl@hotmail.be>
2025-08-26 22:29:30 +0200

    console logging lib

---
+++ logging/console.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+enum LogLevel { DEBUG, INFO, WARNING, ERROR };
+
+class Logger {
+  public:
+    Logger(const bool &logDate = false) { logsDate = logDate; }
+
+    void log(LogLevel level, const std::string &msg) {
+        if (logsDate) {
+            std::cout << get_datetime_string() << " [" << level_to_string(level) << "] " << msg << std::endl;
+        } else {
+            std::cout << "[" << level_to_string(level) << "] " << msg << std::endl;
+        }
+    }
+
+  private:
+    bool logsDate;
+
+    std::string level_to_string(LogLevel level) {
+        switch (level) {
+        case DEBUG:
+            return "DEBUG";
+        case INFO:
+            return "INFO";
+        case WARNING:
+            return "WARNING";
+        case ERROR:
+            return "ERROR";
+        default:
+            return "INVALID_LEVEL";
+        }
+    }
+
+    std::string get_datetime_string() {
+        auto now = std::chrono::system_clock::now();
+        std::time_t t = std::chrono::system_clock::to_time_t(now);
+        auto lt = std::localtime(&t);
+
+        std::ostringstream oss;
+        oss << std::setfill('0') << std::setw(2) << lt->tm_mday << "-" << std::setw(2) << (lt->tm_mon + 1) << "-"
+            << (1900 + lt->tm_year) << " " << std::setw(2) << lt->tm_hour << ":" << std::setw(2) << lt->tm_min << ":"
+            << std::setw(2) << lt->tm_sec;
+
+        return oss.str();
+    }
+};
